7249ad58afc23fe6d3f123ea92aaef7f7cca5416
thingexplainer <utemuka@gmail.com>
2020-08-04 13:17:29 -0700

    fix bug

--- linear_regression/regression_model.py
+++ linear_regression/regression_model.py
@@ -1,5 +1,6 @@
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt # graph the model
+import numpy as np
 
 class Regression_model:
 
@@ -8,7 +9,8 @@ class Regression_model:
         self.dataset = dataset
         self.x = dataset[x].values.reshape(-1, 1)
         self.y = dataset[y].values
-        self.lin_reg = LinearRegression().fit(self.x, self.y)
+        self.lin_reg = LinearRegression()
+        self.lin_reg.fit(self.x, self.y)
 
     def graph(self):
         plt.scatter(self.x, self.y, color = "blue")
@@ -20,9 +22,9 @@ class Regression_model:
         print("type in quit to stop")
         while(not done):
             user_input = input("predict y, type in the x: ")
-            if(user_input == "done"):
+            if(user_input == "quit"):
                 done = True
             else:
-                print(self.lin_reg.predict(user_input))
-
-        
+                y = self.lin_reg.predict(np.array([int(user_input), 0]).reshape(-1, 1))
+                y.flatten()
+                print(round(y[0]))
